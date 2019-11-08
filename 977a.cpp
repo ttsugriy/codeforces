0e0f0c9579cc710fd5d75d82f6db177c9890ff46
Taras Tsugrii <taras.tsugriy@gmail.com>
2019-11-08 14:41:20 -0800

    [977a] C++ solution for problem 977A
    http://codeforces.com/contest/977/problem/A

---
+++ 977a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int n, k; std::cin >> n >> k;
+    for (int i = 0; i < k; ++i) {
+        if (n % 10 == 0) {
+            n /= 10;
+        } else {
+            n -= 1;
+        }
+    }
+    std::cout << n;
+}
